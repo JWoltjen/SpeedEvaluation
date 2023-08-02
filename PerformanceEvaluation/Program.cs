07acce5d2b018b719c3379e19a74fa2ab9ad40c8
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2023-08-02 09:39:23 -0500

    Create CalculateSpeed method to test performance of any method

--- PerformanceEvaluation/Program.cs
+++ PerformanceEvaluation/Program.cs
@@ -19,10 +19,11 @@ namespace PerformanceEvaluation
                 test += "Testing";
             }
             stopwatch.Stop();
-            Console.WriteLine($"Time Taken Using String Concatenation: {stopwatch.ElapsedMilliseconds} ");
+            Console.WriteLine($"Time Taken Using String Concatenation: {stopwatch.ElapsedMilliseconds}ms ");
 
-            stopwatch.Restart();
-            Stopwatch.StartNew();
+            stopwatch = Stopwatch.StartNew();
+            stopwatch.Start();
+
             StringBuilder test1 = new StringBuilder();
 
             for (int i = 0; i < 30000; i++)
@@ -30,10 +31,23 @@ namespace PerformanceEvaluation
                 test1.Append("testing");
             }
             stopwatch.Stop();
-            Console.WriteLine($"Time Taken Using String Builder: {stopwatch.ElapsedMilliseconds}");
+            Console.WriteLine($"Time Taken Using String Builder: {stopwatch.ElapsedMilliseconds}ms");
 
             Console.ReadLine();
+        }
+        //In C#, an Action is a delegate type defined in the System namespace that represents a method containing a void return type and no parameters. It's part of the built-in delegate types that provide a simple way to pass methods as parameters without having to declare a custom delegate
+        public static void CalculateSpeed(Action<int> methodToTest, int[] repetitions, string testName )
+        {
+            for(int i = 0; i < repetitions.Length; i++)
+            {
+                Stopwatch stopwatch = Stopwatch.StartNew();
+                stopwatch.Start();
 
+                methodToTest(repetitions[i]);
+
+                stopwatch.Stop();
+                Console.WriteLine($"{testName} {repetitions[i]} reps:{stopwatch.ElapsedMilliseconds}ms");
+            }
         }
     }
 }
