cf82bc8fabba134b89875629ad3c093a0d7cbb9e
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2023-08-02 10:10:47 -0500

    Refactor to use built in method to test string concatenation and string builder

--- PerformanceEvaluation/Program.cs
+++ PerformanceEvaluation/Program.cs
@@ -8,33 +8,58 @@ namespace PerformanceEvaluation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Speed testing");
-            Stopwatch stopwatch = Stopwatch.StartNew();
+            /*    Console.WriteLine("Speed testing");
+                Stopwatch stopwatch = Stopwatch.StartNew();
 
-            stopwatch.Start();
+                stopwatch.Start();
 
-            string test = "";
-            for(int i = 0; i < 30000; i++)
-            {
-                test += "Testing";
-            }
-            stopwatch.Stop();
-            Console.WriteLine($"Time Taken Using String Concatenation: {stopwatch.ElapsedMilliseconds}ms ");
+                string test = "";
+                for(int i = 0; i < 30000; i++)
+                {
+                    test += "Testing";
+                }
+                stopwatch.Stop();
+                Console.WriteLine($"Time Taken Using String Concatenation: {stopwatch.ElapsedMilliseconds}ms ");
 
-            stopwatch = Stopwatch.StartNew();
-            stopwatch.Start();
+                stopwatch = Stopwatch.StartNew();
+                stopwatch.Start();
 
-            StringBuilder test1 = new StringBuilder();
+                StringBuilder test1 = new StringBuilder();
 
-            for (int i = 0; i < 30000; i++)
-            {
-                test1.Append("testing");
-            }
-            stopwatch.Stop();
-            Console.WriteLine($"Time Taken Using String Builder: {stopwatch.ElapsedMilliseconds}ms");
+                for (int i = 0; i < 30000; i++)
+                {
+                    test1.Append("testing");
+                }
+                stopwatch.Stop();
+                Console.WriteLine($"Time Taken Using String Builder: {stopwatch.ElapsedMilliseconds}ms");*/
+
+            int[] iterations = new int[] { 500, 5000, 50000 };
+
+            CalculateSpeed(AppendText, iterations, "Concatenate Text");
+            CalculateSpeed(StringBuildTest, iterations, "StringBuilder Text");
 
             Console.ReadLine();
         }
+
+        public static void AppendText(int repetitions)
+        {
+            string test = "";
+            for (int i = 0; i < repetitions; i++)
+            {
+                test += "testing";
+            }
+        }
+        public static void StringBuildTest(int repetitions)
+        {
+            StringBuilder test1 = new StringBuilder();
+
+            for (int i = 0; i < repetitions; i++)
+            {
+                test1.Append("testing");
+            }
+            string sbOutput = test1.ToString();
+        }
+
         //In C#, an Action is a delegate type defined in the System namespace that represents a method containing a void return type and no parameters. It's part of the built-in delegate types that provide a simple way to pass methods as parameters without having to declare a custom delegate
         public static void CalculateSpeed(Action<int> methodToTest, int[] repetitions, string testName )
         {
